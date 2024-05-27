caf9c51d8f6fe7f9be9ad5f136be144c484b9e3c
kanbaru-github <usupfv@gmail.com>
2024-05-27 09:11:40 +0900

    Completed column 37

---
+++ PHP8上級_実習環境/Docker/html/column/032.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHP8上級_実習環境\Docker\Html\Column;
+
+use ArrayObject;
+use Countable;
+
+error_reporting(-1);
+
+// 簡単な無名クラス
+$object = new class () {
+    public function test()
+    {
+        var_dump(__METHOD__);
+    }
+};
+$object->test();
+
+// コンストラクタを持つ無名クラス
+$object = new class (123, 'str', [1, 2, 3]) {
+    public function __construct(
+        private int $num,
+        private string $str,
+        private array $arr,
+    ) {
+    }
+};
+var_dump($object);
+
+// 継承
+$object = new class () extends ArrayObject {
+};
+var_dump($object instanceof arrayObject);
+
+// interfaceの継承
+$object = new class () implements Countable {
+    public function count(): int
+    {
+        return 0;
+    }
+};
+var_dump($object instanceof Countable);
+
+// trait
+trait HogeTrait
+{
+    public function hoge()
+    {
+        var_dump(__METHOD__);
+    }
+}
+$object = new class () {
+    use HogeTrait;
+};
+$object->hoge();
+
+// 例えば `get_class()` で確認すると、クラス名を得る事ができます
+var_dump(get_class($object));
